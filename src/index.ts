8fe2cbdda8c7de16ef03f8caa7fdfbf745c1ea17
David Khierl <dkrgidor@gmail.com>
2024-07-22 22:34:43 +0400

    feat: Implement BingoServer functionality
    - Import BingoServer and UserManager in index.ts
    - Create userManager object instance
    - Start server and handle user connections

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import BingoServer from '@/server/bingo-server.ts'
+import UserManager from '@/user-manager.ts'
+
+const userManager = new UserManager()
+
+const server = new BingoServer((token) => (token ? userManager.createUser() : userManager.createAnonymousUser()))
+
+server.start()
+
+server.on('connection', (ws) => {
+    console.log('Client connected', ws.data)
+})
