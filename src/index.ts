936fea26dbcc5c208e105535c327c87f0d7e7cb6
David Khierl <dkrgidor@gmail.com>
2024-07-26 02:05:31 +0400

    refactor: Update Gemo server initialization and event handling
    This commit refactors the code in `src/index.ts` to update the initialization of the Gemo server and improve event handling. The `Server` class is replaced with the `Gemo` class from the `@gemo` library, providing centralized management of the server and rooms. The `auth` method is now defined within the `server` object of the `Gemo.create` function, allowing for better control of user authentication. Additionally, the event handling for user and anonymous messages is simplified and consolidated. These changes enhance the codebase by providing a more organized and efficient way to manage the Gemo server and handle events.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
-import { Server } from '@gemo'
+import { Gemo } from '@gemo'
 import { createUser } from './services/create-user'
 
-const server = new Server({
-    auth(token) {
-        if (token) return createUser()
-        else return null
+const gemo = Gemo.create({
+    server: {
+        auth(token) {
+            if (token) return createUser()
+            else return null
+        },
     },
-})
+}).listen(3000)
 
-server.listen()
+const room = gemo.rooms.create('main', { autoJoin: true })
 
-server.on('userMessage', (ws, message) => {
-    console.log('User message', ws.data.user, JSON.parse(typeof message === 'string' ? message : message.toString()))
-})
-
-server.on('anonymousMessage', (ws, message) => {
-    console.log(
-        'Anonymous message',
-        ws.data.user,
-        JSON.parse(typeof message === 'string' ? message : message.toString())
-    )
+gemo.server.on('message', (ws, message) => {
+    room.send(message)
 })
