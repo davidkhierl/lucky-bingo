888e7f053a6a84b2478f35d2ca16346026ec7e5e
David Khierl <dkrgidor@gmail.com>
2024-07-26 02:04:05 +0400

    feat: Add Rooms class for managing collections of rooms

---
+++ src/lib/gemo/core/rooms.ts
@@ -0,0 +1,38 @@
+import { Room, type RoomOptions, type Server } from '..'
+
+/**
+ * Represents a collection of rooms.
+ * @template U - The type of user in the rooms.
+ */
+export class Rooms<U> {
+    private readonly rooms = new Set<Room<U>>()
+
+    /**
+     * Creates a new room.
+     * @param server - The server instance.
+     * @returns The created room.
+     */
+    constructor(private readonly server: Server<U>) {}
+
+    /**
+     * Creates a new room with the specified name and options.
+     * @param name - The name of the room.
+     * @param options - The options for the room.
+     * @returns The created room.
+     */
+    public create(name: string, options?: Omit<RoomOptions, 'name'>): Room<U> {
+        const room = new Room<U>(this.server, { name, ...options })
+        room.on('dispose', this.handleOnRoomDispose.bind(this))
+        this.rooms.add(room)
+        return room
+    }
+
+    /**
+     * Handles the 'dispose' event of a room.
+     * @param room - The room being disposed.
+     */
+    private handleOnRoomDispose(room: Room<U>) {
+        this.rooms.delete(room)
+        room.off('dispose', this.handleOnRoomDispose.bind(this))
+    }
+}
