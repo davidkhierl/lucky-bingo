a865af85da4594f81a25872f8a3d72b877bf5b49
David Khierl <dkrgidor@gmail.com>
2024-07-28 02:54:09 +0400

    feat: Add control option to TimerEngineOptions
    This commit adds a new control option to the TimerEngineOptions interface. The control option allows for different types of timer engines, such as single or continuous. The type can be specified as 'single' or 'continuous', and an optional delay can be provided for the continuous type. This addition enhances the flexibility and functionality of the TimerEngine class in managing game engines in the gemo library.

--- src/lib/gemo/engines/timer-engine.ts
+++ src/lib/gemo/engines/timer-engine.ts
@@ -1,29 +1,35 @@
-import { scan, takeWhile, timer } from 'rxjs'
+import { identity, repeat, scan, takeWhile, timer } from 'rxjs'
 import { logger, type RoundState } from '..'
 import type { Engine } from './engine'
+
+export type TimerEngineType = { type: 'single' } | { type: 'continues'; delay?: number }
 
 export interface TimerEngineOptions {
     duration: number
     frequency?: number
     lockAt?: number
+    control?: TimerEngineType
 }
 
 export class TimerEngine<U> implements Engine<U> {
     public readonly duration: number
     public readonly frequency: number
     public readonly lockAt: number
+    public readonly control: TimerEngineType
 
     constructor(options: TimerEngineOptions) {
         this.duration = options.duration
         this.frequency = options.frequency || 1000
         this.lockAt = options.lockAt || 0
+        this.control = options.control || { type: 'single' }
     }
 
     public start(round: RoundState<U>) {
         return timer(0, this.frequency)
             .pipe(
                 scan((currentTimer) => --currentTimer, this.duration + 1),
-                takeWhile((timer) => timer >= 0)
+                takeWhile((timer) => timer >= 0),
+                this.control.type === 'continues' ? repeat({ delay: this.control.delay }) : identity
             )
             .subscribe({
                 next: async (timer) => {
