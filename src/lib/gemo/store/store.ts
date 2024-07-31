611ddf484773143aa3b73825178959c09d7a3770
David Khierl <dkrgidor@gmail.com>
2024-08-01 00:53:29 +0400

    refactor(store): convert Store to abstract class and add new methods
    - Convert Store interface to an abstract class with a prefix property.
    - Use kebabCase to format the prefix property.
    - Add constructor to initialize prefix in Store class.
    - Introduce abstract methods decr, delete, get, incr, and set with respective doc comments.
    - Implement keyConstructor method to construct keys using the prefix.

--- src/lib/gemo/store/store.ts
+++ src/lib/gemo/store/store.ts
@@ -1,26 +1,60 @@
-export interface Store {
+import { kebabCase } from 'lodash'
+
+export abstract class Store {
     /**
-     * The prefix to use for keys in the store.
+     * Store prefix.
      */
-    prefix: string
+    public readonly prefix: string
+
+    protected constructor(prefix: string) {
+        this.prefix = kebabCase(prefix)
+    }
+
+    /**
+     * Decreases the value of the specified key by 1 and returns the new value.
+     *
+     * @param {string} key - The key to decrement.
+     * @returns {Promise<number>} - A Promise that resolves with the new value after decrementing, or rejects with an error.
+     */
+    abstract decr(key: string): Promise<number>
+
+    /**
+     * Deletes a value from the store.
+     * @param key - The key to delete.
+     * @returns True if the key was deleted, otherwise false.
+     */
+    abstract delete(key: string): Promise<string | null>
+
+    /**
+     * Gets a value from the store.
+     * @param key - The key to get.
+     * @returns The value if it exists, is otherwise undefined
+     */
+    abstract get(key: string): Promise<string | null>
+
+    /**
+     * Increases the value of the specified key by 1 and returns the new value.
+     *
+     * @param {string} key - The key to increment.
+     * @returns {Promise<number>} - A Promise that resolves with the new value after incrementing, or rejects with an error.
+     */
+    abstract incr(key: string): Promise<number>
 
     /**
      * Sets a value in the store.
      * @param key - The key to set.
      * @param value - The value to set.
+     * @returns The value that was set.
      */
-    set<D = any>(key: string, value: D): D | Promise<D>
+    abstract set(key: string, value: string): Promise<string>
 
     /**
-     * Gets a value from the store.
-     * @param key - The key to get.
-     * @returns The value associated with the key.
+     * Constructs a key using a provided string.
+     *
+     * @param {string} key - The string to construct the key with.
+     * @return {string} The constructed key.
      */
-    get<D = any>(key: string): (D | undefined) | Promise<D | undefined>
-
-    /**
-     * Deletes a value from the store.
-     * @param key - The key to delete.
-     */
-    delete(key: string): boolean | Promise<boolean>
+    protected keyConstructor(key: string): string {
+        return `${this.prefix}:${key}`
+    }
 }
