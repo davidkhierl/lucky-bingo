dd5f993efc9a9087a54d02e8d9750cf6a18403d0
David Khierl <dkrgidor@gmail.com>
2024-08-03 23:51:54 +0400

    refactor(engine): update Engine interface to support generic RoundState
    - Change Engine interface to include additional generic type
    - Update start method in Engine interface to use updated RoundState

--- src/lib/gemo/engines/engine.ts
+++ src/lib/gemo/engines/engine.ts
@@ -2,8 +2,8 @@ import type { Subscription } from 'rxjs'
 import type { Promisable } from 'type-fest'
 import type { RoundState } from '..'
 
-export interface Engine<U> {
-    start(round: RoundState<U>): Promisable<Subscription | void>
+export interface Engine<R, U> {
+    start(round: RoundState<R, U>): Promisable<Subscription | void>
     complete(): void
     destroy(): void
 }
