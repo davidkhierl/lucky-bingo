6bc093a6e95e485bac46962fa80f600dfa9f0312
David Khierl <dkrgidor@gmail.com>
2024-08-04 19:03:36 +0400

    refactor(engine): update Engine interface with new generic type parameter
    - Extend Engine interface with a new generic type parameter B to represent a boolean value.
    - Update start method to use the new generic type parameter in RoundState.
    
    These changes improve the flexibility of the Engine interface for handling different types of rounds.

--- src/lib/gemo/engines/engine.ts
+++ src/lib/gemo/engines/engine.ts
@@ -2,8 +2,8 @@ import type { Subscription } from 'rxjs'
 import type { Promisable } from 'type-fest'
 import type { RoundState } from '..'
 
-export interface Engine<R, U> {
-    start(round: RoundState<R, U>): Promisable<Subscription | void>
+export interface Engine<R, B, U> {
+    start(round: RoundState<R, B, U>): Promisable<Subscription | void>
     complete(): void
     destroy(): void
 }
