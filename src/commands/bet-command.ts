3ad3b3752ea7e9c5d3c7f3da6e0eee409e8fa76c
David Khierl <dkrgidor@gmail.com>
2024-08-04 22:04:09 +0400

    refactor: Update BetCommand to handle user bets
    This commit refactors the BetCommand in the src/commands/bet-command.ts file to handle user bets in a more structured and error-handling manner. It introduces the `betSchema` to validate the incoming bet message and ensures that the quantity of the bet is valid. Additionally, it checks if the user is anonymous before processing the bet. This refactor enhances the functionality of the application for handling user bets in a more robust way.

--- src/commands/bet-command.ts
+++ src/commands/bet-command.ts
@@ -1,8 +1,35 @@
-import { Command, Room, State, type ServerWebSocket } from '@/lib/gemo'
+import { Command, createMessage, messageCommandSchema, Room, type ServerWebSocket } from '@/lib/gemo'
+import type { Bet, User, UserBet } from '@/types'
+import destr from 'destr'
+import { z } from 'zod'
 
-export class BetCommand<U> extends Command<U> {
-    protected async handle(ws: ServerWebSocket<U>, message: string | Buffer, room: Room<U>): Promise<void> {
-        if (room.round.events.value.state === State.Concluded) await room.round.run()
-        else await room.round.conclude()
+export class BetCommand extends Command<User> {
+    protected async handle(
+        ws: ServerWebSocket<User>,
+        message: string | Buffer,
+        room: Room<Bet, UserBet, User>
+    ): Promise<void> {
+        if (ws.data.isAnonymous) return
+        const user = ws.data.user
+
+        const { success, data } = betSchema.safeParse(destr(message))
+
+        if (!success) {
+            ws.send(createMessage(4000, { error: 'Invalid message' }))
+            return
+        }
+
+        if (data.quantity < 1) {
+            ws.send(createMessage(4000, { error: 'Invalid quantity' }))
+            return
+        }
+
+        Array.from({ length: data.quantity }).forEach(() => {
+            room.round.bet({ user, ws })
+        })
     }
 }
+
+export const betSchema = messageCommandSchema.extend({
+    quantity: z.number().int().positive(),
+})
