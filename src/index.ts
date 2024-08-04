68deb33ebfc3872defcb0d4f9943832838c2a3b3
David Khierl <dkrgidor@gmail.com>
2024-08-04 22:03:56 +0400

    refactor: Update Gemo configuration and add StartGameCommand
    This commit updates the Gemo configuration in the src/index.ts file to include the StartGameCommand. It also adds the necessary import statement for the StartGameCommand. This change enhances the functionality of the application by allowing the execution of the StartGameCommand when the corresponding HTTP code is received.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { Gemo, TimerEngine } from '@gemo'
+import { createMessage, Gemo, State, TimerEngine } from '@gemo'
 import { RedisStore } from '@gemo/store/redis-store.ts'
 import { Redis } from 'ioredis'
 import { BetCommand } from './commands/bet-command'
+import { StartGameCommand } from './commands/start-game-command'
 import { LuckyBingoRound } from './round/lucky-bingo-round'
 import { createUser } from './services/create-user'
+import type { Bet } from './types'
 
 const gemo = Gemo.create('Luck Bingo', {
     server: {
@@ -12,32 +14,48 @@ const gemo = Gemo.create('Luck Bingo', {
             else return null
         },
     },
-    commands: [{ code: 200, commands: [BetCommand] }],
+    commands: [
+        { code: 200, commands: [StartGameCommand] },
+        { code: 201, commands: [BetCommand] },
+    ],
     store: new RedisStore(new Redis(), 'Lucky Bingo'),
-}).listen(3000)
+}).listen(4000)
 
-const room = gemo.rooms.create('main', {
+export const room = gemo.rooms.create<Bet, Bet>('main', {
     autoJoin: true,
     round: LuckyBingoRound,
-    engine: new TimerEngine({ duration: 10, lockAt: 5, control: { type: 'single' } }),
+    engine: new TimerEngine({ duration: 181, lockAt: 151, control: { type: 'single' } }),
 })
 
-void room.round.run()
+room.round.events.subscribe((round) => {
+    const roundValues = round.values
 
-// import { concatMap, from } from 'rxjs'
-//
-// function willComplete(duration: number) {
-//     return new Promise<number>((resolve) => {
-//         setTimeout(() => {
-//             resolve(duration)
-//         }, duration)
-//     })
-// }
-//
-// from([willComplete(2000), willComplete(10000), willComplete(3000)])
-//     .pipe(concatMap((promise) => promise))
-//     .subscribe({
-//         next: (value) => console.log('Next', value),
-//         complete: () => console.log('Complete'),
-//         error: (err) => console.error(err),
-//     })
+    if (round.state === State.Concluded) {
+        console.log(round)
+        if (round.result?.user) {
+            room.pool
+                .get(round.result.sessionId)
+                ?.send(
+                    createMessage(103, { result: 'WIN', card: round.result.card.card, cardId: round.result.card.id })
+                )
+        }
+        round.bets?.placed
+            .filter((bet) => bet.card.id !== round.result?.card.id)
+            .forEach((bet) => {
+                room.pool
+                    .get(bet.sessionId)
+                    ?.send(createMessage(103, { result: 'LOSE', card: bet.card.card, cardId: bet.card.id }))
+            })
+    }
+
+    room.send(
+        createMessage(101, {
+            id: roundValues.id,
+            number: roundValues.number,
+            state: roundValues.state,
+            timer: roundValues.timer,
+            draw: round.metadata?.draw,
+            total: round.metadata?.total,
+        })
+    )
+})
